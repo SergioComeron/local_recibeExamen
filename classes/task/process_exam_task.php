2bd512629ad531b3f41db78f55044e726c440671
SergioComeron <sergiocomeron@icloud.com>
2025-05-26 10:38:03 +0200

    Cambio fullname() por firstname lastname

--- classes/task/process_exam_task.php
+++ classes/task/process_exam_task.php
@@ -367,7 +367,7 @@ class process_exam_task extends \core\task\adhoc_task {
             $emailcopiaresult = email_to_user(
                     $copiato,
                     \core_user::get_support_user(), // Aseguramos el namespace global
-                    $subject. ' - Copia '. fullname($user),
+                    $subject. ' - Copia '. $user->username . ' ' . $user->lastname,
                     $message_plain,
                     $message_html,
                     $pdfpath,
